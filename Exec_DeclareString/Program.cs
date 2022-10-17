eef2ab42055f650f2d21eb5d2d276be36b64be46
ispan <ispan@DESKTOP-PAKSETB>
2022-10-17 16:49:26 +0800

    加入專案檔案。

---
+++ Exec_DeclareString/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exce_DeclareString
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.WriteLine("c:\\temp\\a.jpg");
+			Console.WriteLine("我是一個" + "\"" + "好" + "\"" + "學生");
+			Console.WriteLine("我是第一行" + "\r\n" + "我是第二行");
+		}
+	}
+}
